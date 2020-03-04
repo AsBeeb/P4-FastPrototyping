30f2a1fac53e44ddc6d38941232bf729e2529a8b
Michael Lindgaard Nielsen <michni18@student.aau.dk>
2020-03-04 14:14:14 +0100

    Tilføjet at man kan indlæse en fil ud fra en sti til mappen og et navn på tekst filen

--- P4Project/P4Project/Program.cs
+++ P4Project/P4Project/Program.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 namespace P4Project
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Michael er awesome... indskriv sti til mappe:");
+            string sti = Console.ReadLine();
+            Console.WriteLine("Skriv filnavn:");
+            string filnavn = "\\" + Console.ReadLine() + ".txt";
+            using (StreamReader sr = new StreamReader(sti+filnavn)) {
+                string tekst = sr.ReadToEnd();
+                Console.WriteLine(tekst);
+            }
+            Console.Read();
         }
     }
 }
