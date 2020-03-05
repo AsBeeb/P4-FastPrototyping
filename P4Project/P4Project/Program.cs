25dc3c6c84d37ac7666d12ad3126068b9f897d87
Michael Lindgaard Nielsen <michni18@student.aau.dk>
2020-03-05 10:55:15 +0100

    Læser og printer alle ord fra en tekst fil

--- P4Project/P4Project/Program.cs
+++ P4Project/P4Project/Program.cs
@@ -10,6 +10,23 @@ namespace P4Project
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Indskriv sti til mappe:");
+            string sti = Console.ReadLine();
+            Console.WriteLine("Skriv filnavn:");
+            string filnavn = "\\" + Console.ReadLine() + ".txt";
+            using (StreamReader sr = new StreamReader(sti+filnavn)) {
+                while (sr.Peek() > -1 )
+                {
+                    string tekst = "";
+                    while ( ((char)sr.Peek() != ' ' && ((char)sr.Peek()) != '\n') && sr.Peek() > -1)
+                    {
+                        tekst += ((Char)sr.Read()).ToString();
+                    }
+                    if (sr.Peek() > -1) { sr.Read(); }
+                    Console.WriteLine(tekst);
+                } 
+            }
+            Console.Read();
         }
     }
 }
