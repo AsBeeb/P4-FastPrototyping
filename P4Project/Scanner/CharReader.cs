484f31b719c851fdc130523cea6712a6eeab81af
mmoesm <42961608+mmoesm@users.noreply.github.com>
2020-03-18 16:16:31 +0100

    Method hiding

---
+++ P4Project/Scanner/CharReader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ScannerLib
+{
+    class CharReader : StreamReader
+    {
+        public CharReader(string InputFile) : base(InputFile)
+        {
+        }
+
+        new public char Peek()
+        {
+            return (char) base.Peek();
+        }
+
+        public char Advance()
+        {
+            return (char)base.Read();
+        }
+    }
+}
