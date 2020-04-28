367f07108a50b0d37ee5430a02e46ec508fead70
AsBeeb <asbeeb@gmail.com>
2020-04-28 09:38:50 +0200

    Finished up Program.cs
    Cleaned, comments added

--- P4Project/P4Project/Program.cs
+++ P4Project/P4Project/Program.cs
@@ -21,21 +21,7 @@ namespace P4Project
 
             string fileName = args[0];
             string filePath = Directory.GetCurrentDirectory();
-            //Console.WriteLine("Indskriv sti til mappe:");
-            //string sti = Console.ReadLine();
-            //Console.WriteLine("Skriv filnavn:");
-            //string filnavn = "\\" + Console.ReadLine() + ".txt";
-            //string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            //string gitPath = @"\GitHub\P4-FastPrototyping\P4Project\P4Project\KodeEksempler\";
-            //string fileToOpen = "Demo2";
-            //string fileExtension = ".txt";
-            //string filePath = String.Format("{0}{1}{2}{3}", docPath, gitPath, fileToOpen, fileExtension);
-            //string filePath = @"C:\Users\Michael\Source\Repos\P4-FastPrototyping\P4Project\P4Project\KodeEksempler\TurBaseretKampspil.txt";
-            //string filePath = @"C:\Users\Michael\Source\Repos\P4-FastPrototyping\P4Project\P4Project\KodeEksempler\Minesweeper.txt";
-
-            // Print location where project is installed TEMPORARY
-            Console.WriteLine("Current Dir: " + filePath);
-
+      
             // Check for args
             if (args.Length > 0)
             {
@@ -48,7 +34,7 @@ namespace P4Project
             }
             else
             {
-                // If no arguments are given (filename, save option) end.
+                // If no arguments are given (filename, save option) end
                 return;
             }
 
@@ -77,32 +63,33 @@ namespace P4Project
 
             // Semantics
             SymbolTable symbolTable = new SymbolTable();
+            
             DeclarationVisitor dclVisitor = new DeclarationVisitor(symbolTable);
             dclVisitor.Visit(AST);
 
-            var typeVisitor = new TypeVisitor(symbolTable);
+            TypeVisitor typeVisitor = new TypeVisitor(symbolTable);
             typeVisitor.Visit(AST);
 
+            // Codegeneration
             CodeGeneratorVisitor codeGeneratorVisitor = new CodeGeneratorVisitor(symbolTable);
             codeGeneratorVisitor.Visit(AST);
 
+            // If -s parameter is set, save the .cs code
             if (args.Length > 1 && args[1] == "-s" )
             {
                 SaveProgram(codeGeneratorVisitor.CSharpString.ToString(), filePath);
             }
 
+            // After .cs code is generated, run Roslyn compiler
             CSharpCompiler.CompileAndStartConsole(codeGeneratorVisitor.CSharpString);
         }
 
         public static void SaveProgram(string Program, string filePath)
         {
-            //Naming of the file
-            //filePath = filePath.TrimEnd('.', 't', 'x', 't');
             filePath = filePath.Replace(".txt", "");
             filePath += DateTime.Now.Hour.ToString() + DateTime.Now.Minute.ToString() + DateTime.Now.Second.ToString();
             filePath += ".cs";
-            Console.WriteLine(filePath);
-            Console.ReadKey();
+            
             //Creates and writes to the file
             using (StreamWriter SW = File.CreateText(filePath))
             {
